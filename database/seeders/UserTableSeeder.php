605461b8f198aad1c4f22519911808d45a1a3576
Helen Tao <yun.helen.tao@gmail.com>
2022-09-24 10:37:07 +0000

    set admin

--- database/seeders/UserTableSeeder.php
+++ database/seeders/UserTableSeeder.php
@@ -14,10 +14,10 @@ class UserTableSeeder extends Seeder
      */
     public function run()
     {
-        User::factory()->count(50)->create();
-        $user=User::find(1);
-        $user->name = 'Summer';
-        $user->email = 'summer@example.com';
+        //User::factory()->count(50)->create();
+        $user=User::find(54);
+        //$user->name = 'Summer';
+        //$user->email = 'summer@example.com';
         $user->is_admin=true;
         $user->save();
     }
